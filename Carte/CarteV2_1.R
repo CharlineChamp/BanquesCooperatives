de5bf6d1d6fb813fdbd15471a3664538b2b8a7fb
devilbaba <92438534+devilbaba@users.noreply.github.com>
2022-02-16 10:18:06 +0100

    commit

--- Carte/CarteV2_1.R
+++ Carte/CarteV2_1.R
@@ -18,7 +18,7 @@ l <- vector(length = 0)
 #Indices des codes correspondants aux départements d'outre mer
 indices <- c('971','972','973','974','976')
 for(i in indices){
-  l <- c(l,grep(i, fd_c$code, ignore.case = TRUE))
+    l <- c(l,grep(i, fd_c$code, ignore.case = TRUE))
 }
 
 #On retire les départements d'outre mer
@@ -27,8 +27,8 @@ fd <- fd_cnew[3]
 
 #Fusion pour obtenir des zones d'emplois
 fd_cnew_plot <- fd_cnew%>% 
-                group_by(ze2020)%>% 
-                summarize()
+    group_by(ze2020)%>% 
+    summarize()
 
 plot(fd_cnew_plot)
 
@@ -38,12 +38,12 @@ bdd_zese <- cbind(bdd_zese,fd_cnew_plot$geometry)
 #ggplot
 #Construction de la map en ggplot
 map <- ggplot()+
-       geom_sf(data=bdd_zese, aes(fill=`Taux de pauvreté (en %) - Ensemble...6`,geometry=geometry),color='white',size=.2)+
-       scale_fill_viridis_c(option = 'E')+
-       theme_minimal()+
-       theme(panel.background = element_rect(fill = "light blue"))+
-       geom_point(data=sg,aes(x=Longitude,y=Latitude,group=Type,color=Type,label=Banque),size=.6)+
-        labs(fill = "Taux de pauvreté")
+    geom_sf(data=bdd_zese, aes(fill=`Taux de pauvreté (en %) - Ensemble...6`,geometry=geometry),color='white',size=.2)+
+    scale_fill_viridis_c(option = 'E')+
+    theme_minimal()+
+    theme(panel.background = element_rect(fill = "light blue"))+
+    geom_point(data=sg,aes(x=Longitude,y=Latitude,group=Type,color=Type,label=Banque),size=.6)+
+    labs(fill = "Taux de pauvreté")
 map
 #Conversion en plotly
 ggplotly(map)
