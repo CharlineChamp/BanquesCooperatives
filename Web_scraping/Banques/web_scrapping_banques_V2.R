d2407bdf7338a80ad2d196cb2b96c0341f6137b2
CharlineChamp <92438465+CharlineChamp@users.noreply.github.com>
2022-04-04 15:17:04 +0200

    update bp

--- Web_scraping/Banques/web_scrapping_banques_V2.R
+++ Web_scraping/Banques/web_scrapping_banques_V2.R
@@ -132,8 +132,8 @@ liste_adresses[which(liste_adresses=="Ctre Cial Porte de Normandie78200 BUCHELAY
 liste_adresses[which(liste_adresses=="1060, route de l'Aerodrome-Zone AGROPARCRes. l'Esplanade Bat B84140 MONTFAVET")]<-"1060, Route de l'Aerodrome-Zone Agroparc Res. l'Esplanade, Bat B, 84140"
 liste_adresses[which(liste_adresses=="146-146 bis, rue du Point du Jour3 bis, pl Jules Guesde92100 BOULOGNE BILLANCOURT")]<-"47 Rue du Point du Jour, 92100 Boulogne-Billancourt"
 liste_adresses[which(liste_adresses=="entrée en face du ConforamaCtre Cial CORA - RN 1660740 ST MAXIMIN")]<-"282 Av. Gabriel Péri, 83470 Saint-Maximin-la-Sainte-Baume"
-liste_adresses[which(liste_adresses=="3 A, RUE DE BESANÇON25300 DOUBS")]<-"3 A Rue de Besançon 25300 Doubs"
-liste_adresses[which(liste_adresses=="211 BIS, AVE DE VERSAILLES75016 PARIS 16")]<-"211 bis Av. de Versailles 75016 Paris"
+liste_adresses[which(liste_adresses=="3 A, RUE DE BESANÇON25300 DOUBS")]<-"3 Rue de Besançon 25300 Doubs"
+liste_adresses[which(liste_adresses=="211 BIS, AVE DE VERSAILLES75016 PARIS 16")]<-"211 Av. de Versailles 75016 Paris"
 
 
 # Création d'un data frame contenant les adresses modifiées
