fd3551cb7733be27d9d2b336e1db96ec3658d4bc
Antoine7526 <84380611+Antoine7526@users.noreply.github.com>
2022-04-14 14:00:45 +0200

    add V2

--- Analyse/effectif_banques_par_ze_V2.R
+++ Analyse/effectif_banques_par_ze_V2.R
@@ -1,4 +1,4 @@
-library(sp)
+library(spatstat)
 library(readr)
 library(dplyr)
 
@@ -6,29 +6,26 @@ bdd_social_ze2020 <- readxl::read_xlsx("Données/bdd_social_ze2020.xlsx")
 bdd_social_ze2020 <- cbind(bdd_social_ze2020, fd_c$geometry)
 banque <- readr::read_csv("Données/bdd_coordonnees_banques2022.csv")
 
-
 eff_banq_ze <- function(label_banque){
   df <- data.frame()
-  ze <- c()
-  nb_eff <- c()
+  ze <- vector(length = 0)
+  nb_eff <- vector(length = 0)
   banque <- filter(banque,banque$Banque==label_banque)
-  for (i in 1:length(bdd_social_ze2020$`Zone d'emploi 2020`)){
+  mypattern <- ppp(banque[,4], banque[,5],c(min(banque$Longitude),max(banque$Longitude)), c(min(banque$Latitude),max(banque$Latitude)))
+  for (i in 1:length(bdd_social_ze2020[,1])){
     nb <- 0 
     for (j in 1:length(banque$Banque)){
-      point.long <- banque$Longitude[j]
-      point.lat <- banque$Latitude[j]
-      list.long <- c()
-      list.lat <- c()
+      list.long <- vector(length = 0)
+      list.lat <- vector(length = 0)
       for (k in 1:length(bdd_social_ze2020[[35]][[i]])){
         list.long <- c(list.long, bdd_social_ze2020[[35]][[i]][[k]][[1]][,1])
         list.lat <- c(list.lat, bdd_social_ze2020[[35]][[i]][[k]][[1]][,2])
       }
-      eff <- point.in.polygon(point.long, point.lat, list.long,list.lat)
-      if (eff != 0){
-        nb <- nb + 1
-      }
     }
-    ze <- c(ze, bdd_social_ze2020$`Zone d'emploi 2020`[i])
+    # Construction du polygone avec les long/lat récupérées
+    ow <- owin(poly = list(x = list.long,y = list.lat))
+    nb <- mypattern[ow]$n
+    ze <- c(ze, bdd_social_ze2020[,1][i])
     nb_eff <- c(nb_eff, nb)
   }
   df <- data.frame(ze, nb_eff)
@@ -54,6 +51,6 @@ sum(df_bp$nb_eff)
 # BNP Paribas 
 df_bnp <- eff_banq_ze(label_banque = "BNP Paribas")
 sum(df_bp$nb_eff)
-# Concaténation des data frame
 
+# Concaténation des data frame
 df_eff <- rbind(df_bp, df_ca, df_cm, df_sg, df_bnp)
